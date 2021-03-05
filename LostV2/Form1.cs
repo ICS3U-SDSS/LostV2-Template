3a431d86039f3882972ba4169bcbd10e3e14942b
Tony Theodoropoulos <theodot@outlook.com>
2021-03-05 14:01:31 -0500

    update the template code

--- LostV2/Form1.cs
+++ LostV2/Form1.cs
@@ -17,9 +17,10 @@ namespace LostV2
     public partial class Form1 : Form
     {
         // tracks what part of the game the user is at
-        int scene = 0;  
+        int scene = 0;
 
         // random number generator
+        Random randGen = new Random();
 
         public Form1()
         {
@@ -32,16 +33,25 @@ namespace LostV2
         {
             /// check to see what button has been pressed and advance
             /// to the next appropriate scene
+            
             if (e.KeyCode == Keys.M)       //red button press
             {
-
+                if (scene == 0) 
+                {
+                    
+                }
+                else if (scene == 1) { }
+                else if (scene == 2) { }
+                else if (scene == 3) { }
+                else if (scene == 4) { }
+                else if (scene == 5) { }
+                else if (scene == 6) { }
+                else if (scene == 7) { }
             }
-            else if (e.KeyCode == Keys.B)  //blue button press
-            {
 
-            }
 
             /// Display text and game options to screen based on the current scene
+            
             switch (scene)
             {
                 case 0:  //start scene  
@@ -64,7 +74,5 @@ namespace LostV2
                     break;
             }
         }
-
     }
-
 }
